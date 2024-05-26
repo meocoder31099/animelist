51517af5d179867156c88967fbdce8cd20ad9dd4
meocoder <meocoder31099@icloud.com>
2024-05-26 15:58:14 +0700

    fix: vercel invalid runtime

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			runtime: 'nodejs18.x'
+			// runtime: 'nodejs18.x'
 			// out: 'build',
 			// precompress: true,
 			// envPrefix: 'ANICRUSH_'
